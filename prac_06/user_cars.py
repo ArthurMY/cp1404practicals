56eadb66faf7efa4600bc988e5ef9f3c51d7b73d
arthurMY <a13620093868@gmail.com>
2022-12-14 23:16:55 +0800

    initial commit

--- prac_06/user_cars.py
+++ prac_06/user_cars.py
@@ -8,11 +8,20 @@ from prac_06.car import Car
 
 
 def main():
-    """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    # """Demo test code to show how to use car class."""
+    # my_car = Car(180, "ABC")
+    # my_car.drive(30)
+    # print(f"Car has fuel: {my_car.fuel}")
+    # print(my_car)
+
+    limo = Car(100, "Car")  # object first
+    limo.add_fuel(20)  # method second
+    print(f"Car has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
+    limo.add_fuel(199)
+    limo.drive(162)
+    print(f"{limo.name}, fuel={limo.fuel}, odometer={limo.odometer}")
 
 
 main()
